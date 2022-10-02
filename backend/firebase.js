36e9d1d46f1d362aa3a2b6606d99b8fdd1949d61
Alexander Bouzy <90886567+Abzy2020@users.noreply.github.com>
2022-10-02 15:57:34 -0400

    Update firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,12 +2,12 @@ import {initializeApp} from 'firebase/app';
 import {initializeFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD1AuADX13ljsqtRj2128Ih8wGxt_YhwaQ",
-    authDomain: "zbou-2a453.firebaseapp.com",
-    projectId: "zbou-2a453",
-    storageBucket: "zbou-2a453.appspot.com",
-    messagingSenderId: "774583271486",
-    appId: "1:774583271486:web:baa8f0558b2689615e116f",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
